7d4ababdf0c0d35609c4f2cb69b100c4fa4de393
SuelenWagner <siwc94@gmail.com>
2021-04-10 20:43:57 -0300

    :tada: first commit

---
+++ src/main/java/com/hangmangame/hangmangame/controller/CategoryController.java
@@ -0,0 +1,86 @@
+package com.hangmangame.hangmangame.controller;
+
+import com.hangmangame.hangmangame.model.Category;
+import com.hangmangame.hangmangame.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@CrossOrigin(origins = "http://localhost:8080")
+@RestController
+@RequestMapping("/api")
+public class CategoryController {
+
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    @GetMapping("/categories")
+    public ResponseEntity<List<Category>> getAllCategories(@RequestParam(required = false) String name) {
+        try {
+            List<Category> categories = new ArrayList<Category>();
+
+            if (name == null) {
+                categoryRepository.findAll().forEach(categories::add);
+            } else {
+                categoryRepository.findByNameContaining(name).forEach(categories::add);
+            }
+
+            if (categories.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+            return new ResponseEntity<>(categories, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/categories/{id}")
+    public ResponseEntity<Category> getCategoryById(@PathVariable("id") Integer id) {
+        Optional<Category> categoryData = categoryRepository.findById(id);
+
+        if (categoryData.isPresent()) {
+            return new ResponseEntity<>(categoryData.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/categories")
+    public ResponseEntity<Category> createCategory(@RequestBody Category category) {
+        try {
+            Category _category = categoryRepository
+                    .save(new Category(category.getName()));
+            return new ResponseEntity<>(_category, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/categories/{id}")
+    public ResponseEntity<Category> updateCategory(@PathVariable("id") Integer id, @RequestBody Category category) {
+        Optional<Category> categoryData = categoryRepository.findById(id);
+
+        if (categoryData.isPresent()) {
+            Category _category = categoryData.get();
+            _category.setName(category.getName());
+            return new ResponseEntity<>(categoryRepository.save(_category), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/categories/{id}")
+    public ResponseEntity<HttpStatus> deleteCategory(@PathVariable("id") Integer id) {
+        try {
+            categoryRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
---
+++ src/main/java/com/hangmangame/hangmangame/model/Category.java
@@ -0,0 +1,47 @@
+package com.hangmangame.hangmangame.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+@Table(name = "categories", uniqueConstraints={@UniqueConstraint(columnNames="name")})
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @NotEmpty(message = "Nome da categoria não pode estar vazio")
+    @Column(name = "name")
+    private String name;
+
+    public Category() {
+
+    }
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public Category(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" + "id=" + id + ", name='" + name + '\'' + '}';
+    }
+}
