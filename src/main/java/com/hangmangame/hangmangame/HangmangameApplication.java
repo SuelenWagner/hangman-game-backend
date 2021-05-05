8f417189d5e4c2a3416e2128aaf4aa536f75ba26
SuelenWagner <siwc94@gmail.com>
2021-05-05 13:45:39 -0300

    Extends added in main class

--- src/main/java/com/hangmangame/hangmangame/HangmangameApplication.java
+++ src/main/java/com/hangmangame/hangmangame/HangmangameApplication.java
@@ -2,9 +2,10 @@ package com.hangmangame.hangmangame;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HangmangameApplication {
+public class HangmangameApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HangmangameApplication.class, args);
