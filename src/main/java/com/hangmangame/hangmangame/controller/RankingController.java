6ad78752be87451c713255528512f9737a9f62bc
SuelenWagner <siwc94@gmail.com>
2021-05-09 21:26:03 -0300

    tests 4

--- src/main/java/com/hangmangame/hangmangame/controller/RankingController.java
+++ src/main/java/com/hangmangame/hangmangame/controller/RankingController.java
@@ -45,6 +45,7 @@ public class RankingController {
                 return cmp;
             });
 
+            //List<Ranking> rankingList2 = rankingList.stream().limit(10).collect(Collectors.toList());
             List<Ranking> rankingList2 = rankingList.subList(0,10);
 
             return new ResponseEntity<>(rankingList2, HttpStatus.OK);
